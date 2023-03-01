db33213a83c940d8d810b51799f6293dee60060f
Theo <ekunnusitheophilus@gmail.com>
2023-03-01 21:13:18 +0100

    [refactor]:

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { Logo } from './shared'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      App.
+      <Logo/>
     </div>
   )
 }
